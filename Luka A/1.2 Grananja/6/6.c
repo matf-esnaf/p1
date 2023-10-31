53982b613347557085bdea46222430f2c0644091
luka <luka.ajdukovic.la@gmail.com>
2023-10-31 12:50:05 +0100

    Changes to be committed: 	modified:   .DS_Store 	modified:   Luka A/.DS_Store 	modified:   Luka A/1.1 Izrazi/.DS_Store 	modified:   Luka A/1.2 Grananja/6/6.c
     Untracked files:
    	Luka A/1.1 Izrazi/32/.DS_Store
    	Luka A/1.2 Grananja/.DS_Store
    	Luka A/1.2 Grananja/7/
    	Luka A/1.2 Grananja/8/
    	Luka A/compile_all.sh

--- Luka A/1.2 Grananja/6/6.c
+++ Luka A/1.2 Grananja/6/6.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+
+#define H 23
+#define M 60
+
 int main()
 {
 
-	
+	int x, y, hr, min;
 
-return 0:
+	printf("Uneti vreme u formatu hr:min:");
+		scanf("%d%d", &x, &y);
+
+	if(x>=24 || y>=60){
+		printf("Greska: Neispravan unos vremena.");
+	}else{
+		hr=H-x;
+		min=M-y;
+
+	printf("Do ponoci je ostalo %d sati i %d minuta.\n", hr, min);
+	}
+
+return 0;
 }
