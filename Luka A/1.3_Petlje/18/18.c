fdae4056a539f0a5902f6b9abc37df43607c2cfd
luka <luka.ajdukovic.la@gmail.com>
2023-11-09 23:34:09 +0100

    Changes to be committed: 	modified:   Luka A/1.3_Petlje/18/18.c 	new file:   Luka A/1.3_Petlje/19/19.c

--- Luka A/1.3_Petlje/18/18.c
+++ Luka A/1.3_Petlje/18/18.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 int main()
 {
+	float n, n1, brojevi, brojevi2=0, brpr=0;
 
-	
+	printf("Unesite broj n (broj cifara koji zelite da unesete kasnije): ");
+		scanf("%f", &n);
+
+	if(n<=0){
+		printf("Neispravan unos.\n");
+		return 1;
+	}
+
+	printf("Unesite brojeve: \n");
+
+	while(n1<n){
+		scanf("%f", &brojevi);
+		n1++;
+
+	if(brojevi>0 && brojevi2<0 || brojevi<0 && brojevi2>0 || brojevi<0 && brojevi2==0){
+		brpr++;
+	}
+	brojevi2=brojevi;
+	}
+
+	printf("Broj promena znaka iznosi %.0f.\n", brpr);
 
 return 0;
 }
